a2be4f5cc06e9da9b3e26128c158a923123f49d8
Sebastian Schleich <s.schleich@tri-b-oon.de>
2017-03-12 01:00:27 +0100

    updating lable + layered labels

---
+++ clockApp.py
@@ -0,0 +1,45 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Mar 11 23:10:19 2017
+
+@author: swill_000
+"""
+
+import tkinter as tk
+import time
+
+class myClockApp( tk.Tk ):
+    def __init__( self, content ):
+        tk.Tk.__init__( self )
+
+        container = tk.Frame( self )
+        container.pack( side='top', fill='both', expand=True )
+        container.grid_rowconfigure( 0, weight=1 )
+        container.grid_columnconfigure( 0, weight=1 )
+        
+        self.frames={}
+
+        for F in content:
+            frame = F( container, self )
+            self.frames[F] = frame
+            frame.grid( row=0, column=0, sticky='nsew' )
+        
+        self.update_clock( ClockPage )
+        self.mainloop()
+        
+    def show_frame( self, cont ):
+        self.frames[ cont ].tkraise()
+        
+    def update_clock( self, cont ):
+        now = time.strftime('%H:%M:%S' )
+        self.frames[ cont ].clock.configure( text=now )
+        self.after( 1000, self.update_clock, cont )
+        
+class ClockPage( tk.Frame ):
+    
+    def __init__( self, parent, controller ):
+        tk.Frame.__init__( self, parent )
+        self.clock = tk.Label( self, text='' )
+        self.clock.pack( pady=10, padx=10 )
+        
+tstAPP = myClockApp( [ ClockPage ] )
